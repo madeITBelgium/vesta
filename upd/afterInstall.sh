4c935a307748caa3927c89f4dc626e711000dd01
Made I.T <info@madeit.be>
2018-01-15 21:45:36 +0000

    Fix after update and install

--- upd/afterInstall.sh
+++ upd/afterInstall.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -e "/etc/profile.d/vesta.sh" ]; then
+    echo "export VESTA='$VESTA'" > /etc/profile.d/vesta.sh
+    chmod 755 /etc/profile.d/vesta.sh
+    source /etc/profile.d/vesta.sh
+    PATH=$PATH:$VESTA/bin
+    export PATH
+fi
+
 source /etc/profile.d/vesta.sh
 source /usr/local/vesta/func/main.sh
 bash $VESTA/upd/add_plugin.sh
