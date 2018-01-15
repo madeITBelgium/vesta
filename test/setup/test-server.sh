9bc2b9b46501744576ad117191467466e526bdc6
Made I.T <info@madeit.be>
2018-01-15 19:33:12 +0000

    Fix unit test

--- test/setup/test-server.sh
+++ test/setup/test-server.sh
@@ -4,8 +4,6 @@ THISDIR=$(dirname $0)
 dropletIpv4=$(cat $THISDIR/ip_address)
 PASSWORD=$(cat $THISDIR/password)
 
-ssh -i $THISDIR/sshkey.txt root@$dropletIpv4 "ls -la /usr/local/vesta"
-ssh -i $THISDIR/sshkey.txt root@$dropletIpv4 "ls -la /usr/local/vesta/test"
 ssh -i $THISDIR/sshkey.txt root@$dropletIpv4 "bash /usr/local/vesta/test/$1.sh"
 exitcode=$?
 
