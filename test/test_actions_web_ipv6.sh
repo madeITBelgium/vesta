9288e9563df2467858f45815975cbb790e3c1568
Made I.T <info@madeit.be>
2018-01-15 20:34:29 +0000

    Fix bug & testing

--- test/test_actions_web_ipv6.sh
+++ test/test_actions_web_ipv6.sh
@@ -41,35 +41,68 @@ user="testu_123"
 tmpfile=$(mktemp -p /tmp )
 
 #----------------------------------------------------------#
-#                          IPv6                            #
+#                         WEB                              #
 #----------------------------------------------------------#
 
-# Add ipv6 address
-cmd="v-add-sys-ipv6 2001:1620:28:1:b6f:8bca:93:a116 64 $interface $user"
+# Add web domain
+domain="test-abcd.example.com"
+cmd="v-add-web-domain $user $domain 198.18.0.125 2001:1620:28:1:b6f:8bca:93:a111"
 $cmd > $tmpfile 2>&1
-echo_result "IP6: Adding ipv6 2001:1620:28:1:b6f:8bca:93:a116" "$?" "$tmpfile" "$cmd"
-#/usr/nginx/sbin/nginx -t -c /etc/nginx/nginx.conf
-# Add duplicate ipv6
+echo_result "WEB: Adding domain $domain on 2001:1620:28:1:b6f:8bca:93:a111" "$?" "$tmpfile" "$cmd"
+
+# Add duplicate
 $cmd > $tmpfile 2>&1
 if [ "$?" -eq 4 ]; then
     retval=0
 else
     retval=1
 fi
-echo_result "IP: Duplicate ip address check" "$retval" "$tmpfile" "$cmd"
+echo_result "WEB: Duplicate web domain check" "$retval" "$tmpfile" "$cmd"
 
-# Delete ip address
-cmd="v-delete-sys-ipv6 2001:1620:28:1:b6f:8bca:93:a116"
+# Add web domain alias
+cmd="v-add-web-domain-alias $user $domain v3.$domain"
 $cmd > $tmpfile 2>&1
-echo_result "IP6: Deleting ip 2001:1620:28:1:b6f:8bca:93:a116" "$?" "$tmpfile" "$cmd"
+echo_result "WEB: Adding alias v3.$domain" "$?" "$tmpfile" "$cmd"
 
-# Add ip address
-cmd="v-add-sys-ipv6 2001:1620:28:1:b6f:8bca:93:a111 64 $interface $user"
+# Alias duplicate
 $cmd > $tmpfile 2>&1
-echo_result "IP6: Adding ip 2001:1620:28:1:b6f:8bca:93:a111" "$?" "$tmpfile" "$cmd"
+if [ "$?" -eq 4 ]; then
+    retval=0
+else
+    retval=1
+fi
+echo_result "WEB: Duplicate web alias check" "$retval" "$tmpfile" "$cmd"
 
-cmd="v-add-sys-ipv6 2001:1620:28:1:b6f:8bca:93:a112 64 $interface $user"
+# Add web domain stats
+cmd="v-add-web-domain-stats $user $domain webalizer"
 $cmd > $tmpfile 2>&1
-echo_result "IP6: Adding ip 2001:1620:28:1:b6f:8bca:93:a112" "$?" "$tmpfile" "$cmd"
+echo_result "WEB: Enabling webalizer" "$?" "$tmpfile" "$cmd"
+
+# Add web domain stats 
+cmd="v-add-web-domain-stats-user $user $domain test m3g4p4ssw0rd"
+$cmd > $tmpfile 2>&1
+echo_result "WEB: Adding webalizer uzer" "$?" "$tmpfile" "$cmd"
+
+# Suspend web domain
+cmd="v-suspend-web-domain $user $domain"
+$cmd > $tmpfile 2>&1
+echo_result "WEB: Suspending web domain" "$?" "$tmpfile" "$cmd"
+
+# Unsuspend web domain
+cmd="v-unsuspend-web-domain $user $domain"
+$cmd > $tmpfile 2>&1
+echo_result "WEB: Unsuspending web domain" "$?" "$tmpfile" "$cmd"
+
+# Add web domain ssl
+cp $V_TEST/ssl/crt /tmp/$domain.crt
+cp $V_TEST/ssl/key /tmp/$domain.key
+cmd="v-add-web-domain-ssl $user $domain /tmp"
+$cmd > $tmpfile 2>&1
+echo_result "WEB: Adding ssl support" "$?" "$tmpfile" "$cmd"
+
+# Rebuild web domains
+cmd="v-rebuild-web-domains $user"
+$cmd > $tmpfile 2>&1
+echo_result "WEB: rebuilding web domains" "$?" "$tmpfile" "$cmd"
 
 exit $OUTPUT
