7ea7296d2f6ce66994b53ff7a77ffdee99d75944
info@madeit.be <info@madeit.be>
2018-04-24 14:12:50 +0000

    #14

--- test/test_actions_ipv6.sh
+++ test/test_actions_ipv6.sh
@@ -58,6 +58,8 @@ cmd="v-add-sys-ipv6 2001:1620:28:1:b6f:8bca:93:a116 64 $interface $user"
 $cmd > $tmpfile 2>&1
 echo_result "IP6: Adding ipv6 2001:1620:28:1:b6f:8bca:93:a116" "$?" "$tmpfile" "$cmd"
 /usr/sbin/nginx -t -c /etc/nginx/nginx.conf
+ifconfig
+cat /etc/nginx/conf.d/2001:1620:28:1:b6f:8bca:93:a116.conf
 # Add duplicate ipv6
 $cmd > $tmpfile 2>&1
 if [ "$?" -eq 4 ]; then
@@ -81,4 +83,4 @@ cmd="v-add-sys-ipv6 2001:1620:28:1:b6f:8bca:93:a112 64 $interface $user"
 $cmd > $tmpfile 2>&1
 echo_result "IP6: Adding ip 2001:1620:28:1:b6f:8bca:93:a112" "$?" "$tmpfile" "$cmd"
 
-#exit $OUTPUT - IPv6 Bind problem
+exit $OUTPUT
