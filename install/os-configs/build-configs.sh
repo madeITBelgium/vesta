0342a5d276684acb9838a7357d25b211fc450830
Made IT <info@madeit.be>
2018-07-09 07:27:40 +0000

    Fix ubuntu configs

--- install/os-configs/build-configs.sh
+++ install/os-configs/build-configs.sh
@@ -9,7 +9,7 @@
 mkdir rhel debian ubuntu
 mkdir rhel/5 rhel/6 rhel/7
 mkdir debian/7 debian/8 debian/9
-mkdir ubuntu/12.04 ubuntu/12.10 ubuntu/13.04 ubuntu/13.10 ubuntu/14.04 ubuntu/14.10 ubuntu/15.04 ubuntu/15.10 ubuntu/16.04 ubuntu/16.10
+mkdir ubuntu/12.04 ubuntu/12.10 ubuntu/13.04 ubuntu/13.10 ubuntu/14.04 ubuntu/14.10 ubuntu/15.04 ubuntu/15.10 ubuntu/16.04 ubuntu/16.10 ubuntu/17.04 ubuntu/17.10 ubuntu/18.04
 
 
 start_dir=$(dirname $0)
