350d22754522e5b8cd694ef43fa615380524747f
Made I.T <info@madeit.be>
2018-07-13 00:02:45 +0200

    Update build-configs.sh

--- install/os-configs/build-configs.sh
+++ install/os-configs/build-configs.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 #Copy all files to OS and version specific files
-#
-#
-#
-#
 
-mkdir rhel debian ubuntu
-mkdir rhel/5 rhel/6 rhel/7
-mkdir debian/7 debian/8 debian/9
-mkdir ubuntu/12.04 ubuntu/12.10 ubuntu/13.04 ubuntu/13.10 ubuntu/14.04 ubuntu/14.10 ubuntu/15.04 ubuntu/15.10 ubuntu/16.04 ubuntu/16.10 ubuntu/17.04 ubuntu/17.10 ubuntu/18.04
+mkdir -p rhel/5 rhel/6 rhel/7 >/dev/null 2>&1
+mkdir -p debian/7 debian/8 debian/9 >/dev/null 2>&1
+mkdir -p ubuntu/12.04 ubuntu/12.10 ubuntu/13.04 ubuntu/13.10 >/dev/null 2>&1
+mkdir -p ubuntu/14.04 ubuntu/14.10 ubuntu/15.04 ubuntu/15.10 >/dev/null 2>&1
+mkdir -p ubuntu/16.04 ubuntu/16.10 ubuntu/17.04 ubuntu/17.10 >/dev/null 2>&1
+mkdir -p ubuntu/18.04
 
 
 start_dir=$(dirname $0)
@@ -19,11 +17,13 @@ list_os=$(ls | grep -v general | grep -v build)
 
 for OS in $list_os
 do
-	echo $OS
+    echo $OS
     version_list=$(ls $OS | grep -v general)
     for VERSION in $version_list
     do
-        cp -nR $OS/general/* $OS/$VERSION/
-        cp -nR general/* $OS/$VERSION/
+        cp -nR $OS/general/* $OS/$VERSION/ >/dev/null 2>&1
+        cp -nR general/* $OS/$VERSION/ >/dev/null 2>&1
     done
+    rm -rf $OS/general
 done
+rm -rf general
