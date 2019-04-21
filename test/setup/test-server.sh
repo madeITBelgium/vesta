75f53b416baa973f823ddac3d3aec4038606c845
Made IT <info@madeit.be>
2019-04-21 19:19:27 +0000

    Disable code coverage

--- test/setup/test-server.sh
+++ test/setup/test-server.sh
@@ -4,7 +4,8 @@ THISDIR=$(dirname $0)
 dropletIpv4=$(cat $THISDIR/ip_address)
 PASSWORD=$(cat $THISDIR/password)
 
-ssh -i $THISDIR/sshkey.txt root@$dropletIpv4 "/root/kcov/bin/kcov --include-path=/usr/local/vesta /usr/local/vesta/coverage-$1 /usr/local/vesta/test/$1.sh"
+#ssh -i $THISDIR/sshkey.txt root@$dropletIpv4 "/root/kcov/bin/kcov --include-path=/usr/local/vesta /usr/local/vesta/coverage-$1 /usr/local/vesta/test/$1.sh"
+ssh -i $THISDIR/sshkey.txt root@$dropletIpv4 "bash /usr/local/vesta/test/$1.sh"
 exitcode=$?
 
 exit $exitcode
