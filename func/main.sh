289e31aea185b484c5b36bf945caa13ff0cd6a18
Made I.T <info@madeit.be>
2019-10-10 00:18:57 +0200

    #65 - Check mysql version in validation

--- func/main.sh
+++ func/main.sh
@@ -750,7 +750,7 @@ is_date_format_valid() {
 # Database user validator
 is_dbuser_format_valid() {
     exclude="[!|@|#|$|^|&|*|(|)|+|=|{|}|:|,|<|>|?|/|\|\"|'|;|%|\`| ]"
-    if [ 17 -le ${#1} ]; then
+    if [ 17 -le ${#1} ] && [ $(mysql -V | awk '{print $5}' | cut -d. -f1) = 5 ]; then
         check_result $E_INVALID "mysql username can be up to 16 characters long"
 	fi
     if [[ "$1" =~ $exclude ]]; then
