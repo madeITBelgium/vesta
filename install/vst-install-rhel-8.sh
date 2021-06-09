59e0d109eac712a7c17e0ef2793632021859dad5
Made I.T <info@madeit.be>
2021-06-09 22:41:33 +0200

    Update vst-install-rhel-8.sh

--- install/vst-install-rhel-8.sh
+++ install/vst-install-rhel-8.sh
@@ -462,15 +462,13 @@ if [ "$remi" = 'yes' ] && [ ! -e "/etc/yum.repos.d/remi.repo" ]; then
     sed -i "s/enabled=0/enabled=1/g" /etc/yum.repos.d/remi.repo
 fi
 
-if [ "$release" -lt 8 ]; then
-    # Installing Nginx repository
-    nrepo="/etc/yum.repos.d/nginx.repo"
-    echo "[nginx]" > $nrepo
-    echo "name=nginx repo" >> $nrepo
-    echo "baseurl=http://nginx.org/packages/centos/$release/\$basearch/" >> $nrepo
-    echo "gpgcheck=0" >> $nrepo
-    echo "enabled=1" >> $nrepo
-fi
+# Installing Nginx repository
+nrepo="/etc/yum.repos.d/nginx.repo"
+echo "[nginx]" > $nrepo
+echo "name=nginx repo" >> $nrepo
+echo "baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/" >> $nrepo
+echo "gpgcheck=0" >> $nrepo
+echo "enabled=1" >> $nrepo
 
 # Installing Vesta repository
 vrepo='/etc/yum.repos.d/vesta.repo'
